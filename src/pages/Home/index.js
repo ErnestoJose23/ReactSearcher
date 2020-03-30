28d230a5056c6e1cf7c54df3d1a967d9f7f96282
Ernesto Custodio <ernestojosecg@gmail.com>
2020-03-30 12:38:13 +0200

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,61 @@
-import React from "react";
-import { Container, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  TextField,
+  Button
+} from "@material-ui/core";
 
-export default () => (
-  <Container>
-    <Typography variant="h4">Home</Typography>
-  </Container>
-);
+export default () => {
+  const [searchText, setSearchText] = useState("");
+  const handleSearchTextChange = event => {
+    setSearchText(event.target.value);
+  };
+
+  const handleCleanTextClick = event => {
+    console.log(2);
+  };
+  const handleSearchTextClick = event => {
+    console.log(3);
+  };
+
+  return (
+    <Container>
+      <Card>
+        <Grid container>
+          <Grid>
+            <Typography>Welcome to ReactSearcher</Typography>
+          </Grid>
+          <Grid>
+            <label>Icono</label>
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Search.."
+          onChange={handleSearchTextChange}
+        />
+        <Grid>
+          <Button
+            variant="outlined"
+            color="secondary"
+            onClick={handleCleanTextClick}
+          >
+            <i className="far fa-trash-alt" style={{ padding: 5 }}></i>
+            Reset
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={handleSearchTextClick}
+          >
+            <i className="fas fa-search" style={{ padding: 5 }}></i>
+            Search
+          </Button>
+        </Grid>
+      </Card>
+    </Container>
+  );
+};
